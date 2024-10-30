710ffbe3b9f01ed7fc783d4d6d492ed49ab0855a
coolshou <jimmy_yeh@alphanetworks.com>
2024-10-30 11:04:16 +0800

    import 2.16.0

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/aliyunsdkcore/issues
+# Bug-Submit: https://github.com/<user>/aliyunsdkcore/issues/new
+# Changelog: https://github.com/<user>/aliyunsdkcore/blob/master/CHANGES
+# Documentation: https://github.com/<user>/aliyunsdkcore/wiki
+# Repository-Browse: https://github.com/<user>/aliyunsdkcore
+# Repository: https://github.com/<user>/aliyunsdkcore.git
